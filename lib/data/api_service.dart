6444cd1af14bb9fdccc25e2f727c453c8ce23048
Dean Agan <agandfr@gmail.com>
2026-05-07 22:05:01 +1000

    Make some changes to work on iOS Simulator and desktop

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -1,10 +1,19 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:5146';
+    } else {
+      // iOS Simulator and Desktop use localhost
+      return 'http://localhost:5146';
+    }
+  }
+
   Future<List<Map<String, dynamic>>> fetchAccounts() async {
-    // 10.0.2.2 maps to the localhost from the emulator.
-    final url = Uri.parse('http://10.0.2.2:5146/api/Accounts');
+    final url = Uri.parse('$baseUrl/api/Accounts');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
